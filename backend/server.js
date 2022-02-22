f3fb22c0fb6789840ccbd771e54e3152e3281f6c
gardianos <gardianii@hotmail.com>
2022-02-22 17:53:51 +0100

    connected to MongoDb and error handler

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
+const { application } = require("express");
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
+const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 5001;
 
 const app = express();
-
+connectDB();
 app.use("/api/goals", require("./routes/goalRoutes"));
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
