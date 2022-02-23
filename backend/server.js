1ef3e1154c14fb99c0791c0b2a1956bd733f65db
gardianos <gardianii@hotmail.com>
2022-02-23 01:16:51 +0100

    finished goal controller

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,12 @@ const connectDB = require("./config/db");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 5001;
 
-const app = express();
 connectDB();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/goals", require("./routes/goalRoutes"));
 
 app.use(errorHandler);
